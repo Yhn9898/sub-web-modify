0f4169479ee6948c4d990651885c0368c76c5ece
YH <130430690+Yhn9898@users.noreply.github.com>
2025-10-29 18:11:31 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -29,7 +29,7 @@
               </el-form-item>
               <el-form-item label="后端地址:">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
-                  placeholder="houduan.webn.cc" style="width: 100%">   #后端
+                  placeholder="houduan.webn.cc" style="width: 100%">   
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
@@ -1305,5 +1305,6 @@ export default {
   display: none !important;
 }
 </style>
+
 
 
